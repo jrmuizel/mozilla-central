08312a85f9b30738b5ae621b0b043be9728cad97
Kartikaya Gupta <kgupta@mozilla.com>
2012-04-24 00:49:02 -0400

    Remove unused imports

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -56,8 +56,6 @@ import android.os.SystemClock;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
